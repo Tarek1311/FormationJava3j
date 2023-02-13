f8502fc68b5782db0eb4366d30e8f60a6dbb92c9
Khaldi <tarekkhaldi1311@gmail.com>
2023-02-13 10:50:29 +0100

    attributs d'instance et constructeurs

--- FormationJava3j/src/day1/Animal.java
+++ FormationJava3j/src/day1/Animal.java
@@ -1,5 +1,22 @@
 package day1;
 
 public class Animal {
+	// 1)Attributs d'instance (propri�t�s ou caract�ristiques)
+	String nom;
+	int age;
+	
+	// 2)Les contructeurs
+	
+	public Animal()  // le constructeur par d�faut(sans param�tres)
+	{
+		System.out.println("Construction d'un Animal");
+	}
+	
+	public Animal(String nom, int age)
+	{
+		System.out.println("Construction d'un Animal avec deux param�ters");
+		this.nom = nom;
+		this.age = age;
+	}
 
 }
--- FormationJava3j/src/day1/Application.java
+++ FormationJava3j/src/day1/Application.java
@@ -3,8 +3,20 @@ package day1;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello java");// TODO Auto-generated method stub
-
+		//System.out.println("Hello Java");
+		
+		// 1) Cr�ation d'une instance
+		
+		Animal a1 = new Animal(); // appel au contructeur par d�faut
+		
+		Animal a2 = new Animal(); // appel au contructeur par d�faut
+		
+		System.out.println(a1.nom);  //null
+		System.out.println(a1.age);  // 0
+        
+		Animal a3 = new Animal("Rex", 3);
+		System.out.println("Nom : " + a3.nom);
+		System.out.println("Age : " + a3.age);
 	}
 
 }
