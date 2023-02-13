8452bb74c474aa99577485cb004d4b50de64b80c
Khaldi <tarekkhaldi1311@gmail.com>
2023-02-13 10:56:39 +0100

    le type LocalDate

--- FormationJava3j/src/day1/Animal.java
+++ FormationJava3j/src/day1/Animal.java
@@ -1,22 +1,39 @@
 package day1;
 
+import java.time.LocalDate;
+
 public class Animal {
 	// 1)Attributs d'instance (propri�t�s ou caract�ristiques)
 	String nom;
 	int age;
+	LocalDate dateNaissance;
 	
-	// 2)Les contructeurs
+	// 2)Attribut de classe : c'est une propri�t� li�e � la classe
+	static int AgeMax = 150;
 	
+	
+	// 3)Les contructeurs
+	//Constructeur sans param�tres
 	public Animal()  // le constructeur par d�faut(sans param�tres)
 	{
 		System.out.println("Construction d'un Animal");
 	}
-	
-	public Animal(String nom, int age)
+	//Constructeur avec param�tres
+	public Animal(String nom, int age, LocalDate ld)
 	{
 		System.out.println("Construction d'un Animal avec deux param�ters");
 		this.nom = nom;
 		this.age = age;
+		this.dateNaissance = ld;
+	}
+	
+	// 4) Les m�thodes d'instance
+	
+	public void info()
+	{
+		System.out.println("Nom : "+this.nom);
+		System.out.println("Age : "+this.age);
+		System.out.println("Date de naissance : "+this.dateNaissance);
 	}
 
 }
